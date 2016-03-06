b3fb23fbf3acbc93779615894c3041e6aa348d69
Venj Chu <ersaclarke@gmail.com>
2016-03-06 22:16:58 +0800

    Fixed window size

--- Mast Calculator/Form1.Designer.cs
+++ Mast Calculator/Form1.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.InitialHeightTextBox = new System.Windows.Forms.TextBox();
-            this.TotalHeightTextBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
             this.MaximumLoadTextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.CalculateButton = new System.Windows.Forms.Button();
+            this.TotalHeightTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InitialHeightTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
@@ -44,15 +44,15 @@
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.MinimumColumeComboBox = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.HeightDeltaTextBox = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.HeightDeltaTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MinimumColumeComboBox = new System.Windows.Forms.ComboBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
-            this.ChooseButton = new System.Windows.Forms.Button();
             this.DatabasePathTextBox = new System.Windows.Forms.TextBox();
+            this.ChooseButton = new System.Windows.Forms.Button();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -77,41 +77,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Basic Options";
             // 
-            // label1
+            // CalculateButton
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(26, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Initial Height (m)";
-            // 
-            // InitialHeightTextBox
-            // 
-            this.InitialHeightTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.InitialHeightTextBox.Location = new System.Drawing.Point(134, 17);
-            this.InitialHeightTextBox.Name = "InitialHeightTextBox";
-            this.InitialHeightTextBox.Size = new System.Drawing.Size(185, 23);
-            this.InitialHeightTextBox.TabIndex = 1;
-            // 
-            // TotalHeightTextBox
-            // 
-            this.TotalHeightTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.TotalHeightTextBox.Location = new System.Drawing.Point(134, 44);
-            this.TotalHeightTextBox.Name = "TotalHeightTextBox";
-            this.TotalHeightTextBox.Size = new System.Drawing.Size(185, 23);
-            this.TotalHeightTextBox.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(28, 47);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Total Height (m)";
+            this.CalculateButton.Location = new System.Drawing.Point(325, 16);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(80, 80);
+            this.CalculateButton.TabIndex = 6;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
             // 
             // MaximumLoadTextBox
             // 
@@ -131,14 +104,41 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Maximum Load (kg)";
             // 
-            // CalculateButton
+            // TotalHeightTextBox
             // 
-            this.CalculateButton.Location = new System.Drawing.Point(325, 16);
-            this.CalculateButton.Name = "CalculateButton";
-            this.CalculateButton.Size = new System.Drawing.Size(80, 80);
-            this.CalculateButton.TabIndex = 6;
-            this.CalculateButton.Text = "Calculate";
-            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.TotalHeightTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TotalHeightTextBox.Location = new System.Drawing.Point(134, 44);
+            this.TotalHeightTextBox.Name = "TotalHeightTextBox";
+            this.TotalHeightTextBox.Size = new System.Drawing.Size(185, 23);
+            this.TotalHeightTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(28, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Total Height (m)";
+            // 
+            // InitialHeightTextBox
+            // 
+            this.InitialHeightTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.InitialHeightTextBox.Location = new System.Drawing.Point(134, 17);
+            this.InitialHeightTextBox.Name = "InitialHeightTextBox";
+            this.InitialHeightTextBox.Size = new System.Drawing.Size(185, 23);
+            this.InitialHeightTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(26, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Initial Height (m)";
             // 
             // groupBox2
             // 
@@ -239,6 +239,36 @@
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Other Options";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(138, 78);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(124, 21);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Optimize Results";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // HeightDeltaTextBox
+            // 
+            this.HeightDeltaTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.HeightDeltaTextBox.Location = new System.Drawing.Point(138, 49);
+            this.HeightDeltaTextBox.Name = "HeightDeltaTextBox";
+            this.HeightDeltaTextBox.Size = new System.Drawing.Size(132, 23);
+            this.HeightDeltaTextBox.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(19, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Height Delta (mm)";
+            // 
             // MinimumColumeComboBox
             // 
             this.MinimumColumeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
@@ -261,36 +291,6 @@
             this.MinimumColumeComboBox.Size = new System.Drawing.Size(132, 25);
             this.MinimumColumeComboBox.TabIndex = 14;
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label5.Location = new System.Drawing.Point(19, 52);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(114, 17);
-            this.label5.TabIndex = 15;
-            this.label5.Text = "Height Delta (mm)";
-            // 
-            // HeightDeltaTextBox
-            // 
-            this.HeightDeltaTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.HeightDeltaTextBox.Location = new System.Drawing.Point(138, 49);
-            this.HeightDeltaTextBox.Name = "HeightDeltaTextBox";
-            this.HeightDeltaTextBox.Size = new System.Drawing.Size(132, 23);
-            this.HeightDeltaTextBox.TabIndex = 7;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(138, 78);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(124, 21);
-            this.checkBox1.TabIndex = 16;
-            this.checkBox1.Text = "Optimize Results";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
             // groupBox5
             // 
             this.groupBox5.Controls.Add(this.DatabasePathTextBox);
@@ -305,18 +305,23 @@
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Mast Database";
             // 
-            // radioButton5
+            // DatabasePathTextBox
             // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Checked = true;
-            this.radioButton5.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.radioButton5.Location = new System.Drawing.Point(6, 22);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(67, 21);
-            this.radioButton5.TabIndex = 12;
-            this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Default";
-            this.radioButton5.UseVisualStyleBackColor = true;
+            this.DatabasePathTextBox.Enabled = false;
+            this.DatabasePathTextBox.Location = new System.Drawing.Point(149, 45);
+            this.DatabasePathTextBox.Name = "DatabasePathTextBox";
+            this.DatabasePathTextBox.Size = new System.Drawing.Size(121, 23);
+            this.DatabasePathTextBox.TabIndex = 15;
+            // 
+            // ChooseButton
+            // 
+            this.ChooseButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ChooseButton.Location = new System.Drawing.Point(82, 45);
+            this.ChooseButton.Name = "ChooseButton";
+            this.ChooseButton.Size = new System.Drawing.Size(61, 23);
+            this.ChooseButton.TabIndex = 14;
+            this.ChooseButton.Text = "Choose";
+            this.ChooseButton.UseVisualStyleBackColor = true;
             // 
             // radioButton6
             // 
@@ -330,23 +335,18 @@
             this.radioButton6.Text = "Custom";
             this.radioButton6.UseVisualStyleBackColor = true;
             // 
-            // ChooseButton
+            // radioButton5
             // 
-            this.ChooseButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.ChooseButton.Location = new System.Drawing.Point(82, 45);
-            this.ChooseButton.Name = "ChooseButton";
-            this.ChooseButton.Size = new System.Drawing.Size(61, 23);
-            this.ChooseButton.TabIndex = 14;
-            this.ChooseButton.Text = "Choose";
-            this.ChooseButton.UseVisualStyleBackColor = true;
-            // 
-            // DatabasePathTextBox
-            // 
-            this.DatabasePathTextBox.Enabled = false;
-            this.DatabasePathTextBox.Location = new System.Drawing.Point(149, 45);
-            this.DatabasePathTextBox.Name = "DatabasePathTextBox";
-            this.DatabasePathTextBox.Size = new System.Drawing.Size(121, 23);
-            this.DatabasePathTextBox.TabIndex = 15;
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Checked = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton5.Location = new System.Drawing.Point(6, 22);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(67, 21);
+            this.radioButton5.TabIndex = 12;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Default";
+            this.radioButton5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -358,8 +358,10 @@
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Mast Calculator";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
